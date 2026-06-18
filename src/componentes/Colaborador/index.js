4e13d0771b62dec073eec88cde280dfcce686906
Fernando Henrique <fernando.henrique14@outlook.com>
2026-06-17 23:51:02 -0300

    docs: substituir conteúdo padrão do CRA por descrição do projeto
    Simplificar o README removendo o conteúdo padrão do Create React App e adicionando uma breve descrição  indicando que este é o primeiro projeto web em React.

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,21 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img
+          src="https://github.com/FernandoHenrique900.png"
+          alt="Fernando Henrique"
+        />
+      </div>
+
+      <div className="rodape">
+        <h4>Fernando Henrique</h4>
+        <h5>Cientista</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
